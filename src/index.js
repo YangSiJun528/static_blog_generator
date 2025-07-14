d4529db6d8fc53a8a95d9b3d7dbd41634ce52cc4
sijun-yang <yangsijun5528@gmail.com>
2025-07-14 11:05:52 +0900

    gitignore 대상 파일 삭제처리

--- src/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const generator_1 = require("./generator");
-// Run the generator with the current working directory as the project root
-(0, generator_1.generateStaticBlog)(process.cwd());
