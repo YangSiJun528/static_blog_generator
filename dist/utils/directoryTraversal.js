2324438148fc1fc0da80e772726a8bd785c94ca8
sijun-yang <yangsijun5528@gmail.com>
2025-07-17 17:34:41 +0900

    파일 검증 제거
    없어도 잘 돌아감. 검증 굳이 할 필요 없을 듯, 나만 쓰는건데 뭐

--- dist/utils/directoryTraversal.js
+++ dist/utils/directoryTraversal.js
@@ -36,11 +36,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.traverseDirectory = traverseDirectory;
 const fs = __importStar(require("fs"));
 const path = __importStar(require("path"));
-const ALLOWED_EXTENSIONS = new Set([
-    '.jpg', '.jpeg', '.png', '.gif', '.bmp', '.svg', '.webp',
-    '.mp4', '.mov', '.avi', '.mkv',
-    '.zip', '.tar', '.gz', '.rar'
-]);
 function traverseDirectory(dirPath, callback) {
     fs.readdirSync(dirPath, { withFileTypes: true }).forEach(dirent => {
         const fullPath = path.join(dirPath, dirent.name);
@@ -48,15 +43,6 @@ function traverseDirectory(dirPath, callback) {
             traverseDirectory(fullPath, callback);
         }
         else if (dirent.isFile()) {
-            const relativePath = path.relative(process.cwd(), fullPath);
-            const ext = path.extname(fullPath).toLowerCase();
-            if (ext !== '.md') {
-                if (!relativePath.startsWith('content/files')) {
-                    if (!ALLOWED_EXTENSIONS.has(ext)) {
-                        throw new Error(`Disallowed file outside content/files: ${relativePath}`);
-                    }
-                }
-            }
             callback(fullPath);
         }
     });
--- src/utils/directoryTraversal.ts
+++ src/utils/directoryTraversal.ts
@@ -1,28 +1,12 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
-const ALLOWED_EXTENSIONS = new Set([
-    '.jpg', '.jpeg', '.png', '.gif', '.bmp', '.svg', '.webp',
-    '.mp4', '.mov', '.avi', '.mkv',
-    '.zip', '.tar', '.gz', '.rar'
-]);
-
 export function traverseDirectory(dirPath: string, callback: (filePath: string) => void): void {
     fs.readdirSync(dirPath, { withFileTypes: true }).forEach(dirent => {
         const fullPath = path.join(dirPath, dirent.name);
         if (dirent.isDirectory()) {
             traverseDirectory(fullPath, callback);
         } else if (dirent.isFile()) {
-            const relativePath = path.relative(process.cwd(), fullPath);
-            const ext = path.extname(fullPath).toLowerCase();
-
-            if (ext !== '.md') {
-                if (!relativePath.startsWith('content/files')) {
-                    if (!ALLOWED_EXTENSIONS.has(ext)) {
-                        throw new Error(`Disallowed file outside content/files: ${relativePath}`);
-                    }
-                }
-            }
             callback(fullPath);
         }
     });
