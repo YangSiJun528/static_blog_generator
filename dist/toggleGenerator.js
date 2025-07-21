895664ea840e6429b19a52acb6cf6258148f3836
sijun-yang <yangsijun5528@gmail.com>
2025-07-21 18:14:42 +0900

    gitignore 수정

--- dist/toggleGenerator.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.addToggleFunctionality = addToggleFunctionality;
-const toggle_1 = require("./client/toggle");
-function addToggleFunctionality(htmlContent) {
-    const script = (0, toggle_1.getToggleScript)();
-    // Inject the script before the closing </body> tag
-    return htmlContent.replace('</body>', `${script}</body>`);
-}
